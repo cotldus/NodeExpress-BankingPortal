05a9c4532d2b0b9584de0affc6a5c7f8420c78c9
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-07-03 22:02:52 +0800

    mod 3

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const { render } = require('ejs');
 const app = new express();
 
 app.set('views',path.join(__dirname, '/views'));
@@ -13,6 +14,8 @@ const accountData = fs.readFileSync(accountPath, 'utf8');
 const accounts = JSON.parse(accountData);
 
 app.use(express.static(path.join(__dirname, '/public')));
+//post
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res)=> res.render('index', { title: 'Account Summary', accounts: accounts  }));
 //src/json/users.json
@@ -21,12 +24,29 @@ const userData = fs.readFileSync(userPath, 'utf8');
 const users= JSON.parse(userData);
 
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
-
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) =>{
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount);
+    let accountsJSON = JSONstringify(accounts);
+    accountsJSONPath = path.join(__dirname, 'json', 'accounts.json');
+    fs.writeFileSync(accountsJSONPath, accountsJSON, 'utf8');
+    res.render('transfer', {message: "Transfer Completed"});
+})
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) =>{
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSONstringify(accounts);
+    accountsJSONPath = path.join(__dirname, 'json', 'accounts.json');
+    fs.writeFileSync(accountsJSONPath, accountsJSON, 'utf8');
+    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+})
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!') });
