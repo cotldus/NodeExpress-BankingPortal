fad8437fb3ff6c88f5652330513bb6e373f0ccae
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-07-04 23:27:41 +0800

    mod 5

--- src/app.js
+++ src/app.js
@@ -6,35 +6,21 @@ const express = require('express');
 const { render } = require('ejs');
 const app = new express();
 
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js')
+
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 //post
 app.use(express.urlencoded({extended: true}));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res)=> res.render('index', { title: 'Account Summary', accounts: accounts  }));
 
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
-
-app.get('/transfer', (req, res) => res.render('transfer'));
-app.post('/transfer', (req, res) =>{
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer', {message: "Transfer Completed"});
-})
-
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) =>{
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {message: "Payment Successful", account: accounts.credit});
-})
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!') });
